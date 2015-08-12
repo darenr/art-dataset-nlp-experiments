36ba9e4de581d605c740d8b808aa254ba299f5be
darenr <daren.race@gmail.com>
2015-08-12 16:55:35 -0700

    reformat 2 spaces

--- load_elastic_search.py
+++ load_elastic_search.py
@@ -7,21 +7,21 @@ es = Elasticsearch()
 
 
 def load_data(index):
-    if es.indices.exists(index=index):
-        print es.indices.delete(index=index)
-    print es.indices.create(index=index)
+  if es.indices.exists(index=index):
+    print es.indices.delete(index=index)
+  print es.indices.create(index=index)
 
-    with open('MergedArtworks.csv', 'rb') as in_csv:
-        for m in unicodecsv.DictReader(in_csv, encoding='utf-8'):
-            id = m['ObjectID']
-            if not m['DateAcquired']:
-                m['DateAcquired'] = '2015-01-01'
-            try:
-                es.index(index=index, doc_type='moma_art_collection', id=id, body=m)
-            except KeyboardInterrupt:
-                raise
-            except:
-                print m
+  with open('MergedArtworks.csv', 'rb') as in_csv:
+    for m in unicodecsv.DictReader(in_csv, encoding='utf-8'):
+      id = m['ObjectID']
+      if not m['DateAcquired']:
+        m['DateAcquired'] = '2015-01-01'
+      try:
+        es.index(index=index, doc_type='moma_art_collection', id=id, body=m)
+      except KeyboardInterrupt:
+        raise
+      except:
+        print m
 
 
 load_data('moma')
