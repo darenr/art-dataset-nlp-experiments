f0a0cb8dcea72031d374eac27216d703148d006b
darenr <daren.race@gmail.com>
2015-08-12 10:17:04 -0700

    added preview generator (for github) to csv merger

--- merge_csv.py
+++ merge_csv.py
@@ -4,12 +4,11 @@ import codecs
 import requests
 import os
 import time
-from textblob import TextBlob
 import re
-import fractions
 
 
 results = []
+preview = []
 
 def enrich(m):
   r = re.search(r'\((\w+).*$', m['ArtistBio'])
@@ -17,10 +16,9 @@ def enrich(m):
     m['ArtistNationality'] = r.group(1).upper()
 
 with open('Artworks.csv', 'rb') as in_csv:
-  for m in unicodecsv.DictReader(in_csv, encoding='utf-8'):
+  for m in unicodecsv.DictReader(in_csv, encoding = 'utf-8'):
     url = m['URL']
     oid = m['ObjectID']
-
     # augment with text processing
     enrich(m)
 
@@ -30,15 +28,26 @@ with open('Artworks.csv', 'rb') as in_csv:
       with open(fname) as f:
         m['HasExtraText'] = 'Y'
         m['ExtraText'] = unicode(f.read().replace('\n', ' ').replace('\r', ''), 'utf-8')
-    else:
+        if len(preview) < 50:
+          preview.append(m)
+    else :
       m['HasExtraText'] = 'N'
     results.append(m)
 
 with open('MergedArtworks.csv', 'wb') as out_csv:
-  wr = unicodecsv.DictWriter(out_csv, 
-        encoding='utf-8', 
-        quoting=unicodecsv.QUOTE_ALL, 
-        fieldnames = results[0].keys())
+  wr = unicodecsv.DictWriter(out_csv,
+    encoding = 'utf-8',
+    quoting = unicodecsv.QUOTE_ALL,
+    fieldnames = results[0].keys())
   wr.writeheader()
   for row in results:
+    wr.writerow(row)
+
+with open('MergedArtworksPreview.csv', 'wb') as out_csv:
+  wr = unicodecsv.DictWriter(out_csv,
+    encoding = 'utf-8',
+    quoting = unicodecsv.QUOTE_ALL,
+    fieldnames = results[0].keys())
+  wr.writeheader()
+  for row in preview:
     wr.writerow(row)
