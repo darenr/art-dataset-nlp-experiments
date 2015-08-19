a418cc570d9647f28b2f90c0e3c040e62800ee66
daren race <daren.race@gmail.com>
2015-08-18 21:16:24 -0700

    removed keywords, keep only concepts

--- concept_tag_alchemy.py
+++ concept_tag_alchemy.py
@@ -13,33 +13,23 @@ with open('MOMA3k.csv', 'rb') as in_csv:
   rows = unicodecsv.reader(in_csv, encoding='utf-8')
   fieldnames = rows.next()
 
-  if 'AlchemyKeywords' not in fieldnames:
-    fieldnames.extend(['AlchemyKeywords'])
-  if 'AlchemyConcepts' not in fieldnames:
-    fieldnames.extend(['AlchemyConcepts'])
-
   for line in rows:
     m = OrderedDict([(k,line[i]) for i,k in enumerate(fieldnames)])
-    if not stop and  not 'AlchemyConcepts' in m:
-      txt = m['ExtraText'].encode('ascii', 'ignore')
-      print 'tagging: "', txt, '"'
-      keywords = alchemyapi.keywords('text', txt)
+    if not stop:
+      txt = m['ExtraText'].encode('ascii', 'ignore').strip()
+      print 'tagging:', txt
       concepts = alchemyapi.concepts('text', txt)
-      if keywords['status'] == 'OK' and concepts['status'] == 'OK':
-        m['AlchemyKeywords'] = ', '.join(["{0} ({1})".format(k['text'].encode('ascii', 'ignore'), k['relevance']) 
-          for k in keywords['keywords']])
-        m['AlchemyConcepts'] = ', '.join(["{0} ({1})".format(k['text'].encode('ascii', 'ignore'), k['relevance']) 
-          for k in concepts['concepts']])
+      if concepts['status'] == 'OK':
+        m['AlchemyConcepts'] = ', '.join(["{0} ({1})".format(k['text'].encode('ascii', 'ignore'), k['relevance']) for k in concepts['concepts']])
       else:
-        print('Error in concept tagging call: ', keywords['status'])
+        print(concepts)
         stop = True
     results.append(m)
   in_csv.close()
 
 with open('MOMA3k.csv', 'wb') as out_csv:
-  output = unicodecsv.DictWriter(out_csv, fieldnames=fieldnames, quoting=unicodecsv.QUOTE_ALL)
+  output = unicodecsv.DictWriter(out_csv, fieldnames=fieldnames, lineterminator='\n', quoting=unicodecsv.QUOTE_ALL)
   output.writerow(dict((fn,fn) for fn in fieldnames))
-  for i, row in enumerate(results):
+  for row in results:
     output.writerow(row)
-    if i % 10 == 0:
-      out_csv.flush()
+    out_csv.flush()
