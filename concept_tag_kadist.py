1f8fa901014d1f15e7e943d2a7258f1d01291fe9
darenr <daren.race@gmail.com>
2015-08-21 10:58:51 -0700

    sync in latest experiments

---
+++ concept_tag_kadist.py
@@ -0,0 +1,82 @@
+# -*- encoding: utf-8 -*-
+import base64
+import json
+import os
+import sys
+import codecs
+
+if len(sys.argv) != 3:
+  print "usage: <kadist corpus json>"
+  sys.exit(-1)
+
+def load_json():
+  with codecs.open(sys.argv[1], 'rb', 'utf-8') as f:
+    data = json.loads(f.read())
+    return data
+
+def save_json(data):
+  with codecs.open(sys.argv[2], 'wb', 'utf-8') as f:
+    f.write(html)
+
+
+html = u'''
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+  <style>
+    body {{
+      margin: 20px;
+    }}
+  </style>
+
+  </head>
+  <body>
+    <h1>{0}</h1>
+
+    {1}
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </body>
+</html>
+'''
+
+j = load_json()
+
+elements = []
+
+for row in j:
+  
+  if row['major_tags'] and row['worktype'] and row['description'] and row['imgurl']:
+    tags = ' '.join(['<span class="label label-primary">{0}</span>'.format(tag) for tag in row['major_tags']])
+    elements.append(u'''<div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">{0} - {1} [Ref: {2}]</h3>
+      </div>
+      <div class="panel-body">
+        <img src="{3}" class="thumbnail">
+        <div class="caption"><h2><small>{4}</small></h2></div>
+        {5}
+        <h3>{6}</h3>
+      </div>
+    </div>'''.format(row['title'], 
+                     row['year'], 
+                     row['id'], 
+                     row['imgurl'],
+                     row['worktype'],
+                     row['description'],
+                     tags))
+
+save_web(html.format('Kadist - AlchemyAPI Concept Tagged', '\n'.join(elements)))
