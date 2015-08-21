02d8061c8e512916405eb38f06c0bc53d5035eb2
daren race <daren.race@gmail.com>
2015-08-20 22:04:06 -0700

    fixed up xlreader and got the encoding issues resolved

--- xlreader.py
+++ xlreader.py
@@ -1,21 +1,38 @@
 # -*- encoding: utf-8 -*-
 import xlrd
+import json
 import csv
 import sys
 import codecs
-
 
 wb = xlrd.open_workbook(sys.argv[1])
 sh = wb.sheet_by_name(sys.argv[2])
 
 fieldnames = sh.row_values(0)
 
-with codecs.open('kadist_descriptions.txt', 'wb', 'utf-8') as out:
+data = []
+
+def clean(v):
+  if type(v) is str: 
+    return unicode(v, "utf-8").strip().replace(u'—', ' ').replace('\n', ' ')
+  else:
+    if type(v) is float and v == int(v):
+      v = int(v)
+    return v
+
+z = {}
+
+with codecs.open('kadist.json', 'wb', 'utf-8') as out:
   for rownum in xrange(1, sh.nrows):
-    row = dict(zip(fieldnames, sh.row_values(rownum)))
-    if 'description' in row and row['description']:
-      desc = row['description'].strip().replace(u'—', ' ').replace('\n', ' ').encode('ascii', 'ignore')
-      if desc:
-        out.write(desc + '\n')
-   
+    d = dict(zip(fieldnames, 
+          [clean(x) for x in sh.row_values(rownum)]))
+    if d['id'] == 962:
+      z = d
+      print d['description']
+      print type(d['id'])
+    data.append(d)
+  out.write(json.dumps(data, indent=2, ensure_ascii=False))
+
+print json.dumps(z, indent=2, ensure_ascii=False)
+  
 
