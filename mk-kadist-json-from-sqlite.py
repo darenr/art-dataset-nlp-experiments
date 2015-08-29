97f77bddcbe8060e1067541b720ed647d7b05fb7
daren race <daren.race@gmail.com>
2015-08-29 15:45:46 -0700

    first working sql dump to json conversion

--- mk-kadist-json-from-sqlite.py
+++ mk-kadist-json-from-sqlite.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import sqlite3 as db
+from collections import defaultdict
 import json
 import sys
 import codecs
@@ -22,10 +23,12 @@ def save_json(data):
 copydict = lambda dct, *keys: {key: dct[key] for key in keys}
 
 
-artists = {}
-minor_tags = {}
-major_tags = {}
-works = {}
+artists = {} # keyed by artist ID
+major_tags = {} # keyed on tag ID
+major_tags_rel = defaultdict(list) # keyed by work ID
+minor_tags = {} # keyed on tag ID
+minor_tags_rel = defaultdict(list) # keyed by work ID
+works = []
 
 try:
   con = db.connect(sys.argv[1])
@@ -44,21 +47,45 @@ try:
     for row in cur.fetchall():
       major_tags[row['id']] = {'name': row['name']}
 
+    # load the major tag to works relationship
+    cur.execute("SELECT * FROM kadist_majortaggeditem")
+    for row in cur.fetchall():
+      major_tags_rel[row['object_id']].append(row['tag_id'])
+
     # load the minor_tags
     cur.execute("SELECT * FROM taggit_tag")
     for row in cur.fetchall():
       minor_tags[row['id']] = {'name': row['name']}
 
+    # load the minor tag to works relationship
+    cur.execute("SELECT * FROM taggit_taggeditem")
+    for row in cur.fetchall():
+      minor_tags_rel[row['object_id']].append(row['tag_id'])
+
     # load the works
     cur.execute("SELECT * FROM kadist_work")
     for row in cur.fetchall():
-      d = copydict(row, 'title', 'year', 'imgurl', 'worktype', 'technique', 'dimensions', 'description')
+      id = row['id']
+      d = copydict(row, 'id', 'title', 'year', 'imgurl', 'worktype', 'technique', 'dimensions', 'description')
       # resolve artist details
       if row['creator_id'] and row['creator_id'] in artists:
         d['artist'] = artists[row['creator_id']]
-      # resolve major_tags
 
-      works[row['id']] = d
+      # resolve major_tags
+      d['major_tags'] = []
+      if id in major_tags_rel:
+        for tag in major_tags_rel[id]:
+          if tag in major_tags:
+            d['major_tags'].append(major_tags[tag]['name'])
+
+      # resolve minor
+      d['minor_tags'] = []
+      if id in minor_tags_rel:
+        for tag in minor_tags_rel[id]:
+          if tag in minor_tags:
+            d['minor_tags'].append(minor_tags[tag]['name'])
+
+      works.append(d)
 
     save_json(works)
 
