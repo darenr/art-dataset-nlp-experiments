7a34b6bdb6d1dd8625877856fb9438261c02ed32
daren race <daren.race@gmail.com>
2015-09-05 17:44:42 -0700

    started search webapp

---
+++ webapp/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+ 
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+
+    title = "Epic Tutorials"
+    paragraph = "wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!","wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!"
+
+    try:
+        return render_template("index.html", title = title, paragraph=paragraph)
+    except Exception, e:
+        return str(e)
+
+@app.route('/about/contact')
+def contactPage():
+
+    title = "About this site"
+    paragraph = ["blah blah blah memememememmeme blah blah memememe"]
+
+    pageType = 'about'
+
+    return render_template("index.html", title=title, paragraph=paragraph, pageType=pageType)
+
+
+ 
+if __name__ == "__main__":
+  app.run(debug = True, host='0.0.0.0', port=5000, passthrough_errors=True)
