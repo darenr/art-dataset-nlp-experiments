26503f0b476bc3a4e1dcc0207d4ec9d4333f4026
daren race <daren.race@gmail.com>
2015-09-05 22:53:28 -0700

    finished tinkering for today

--- webapp/app.py
+++ webapp/app.py
@@ -5,24 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     title = "Kadist"
-    paragraph = ["wow I am learning so much great stuff!", "wow I am learning so much great stuff!", "wow I am learning so much great stuff!", "wow I am learning so much great stuff!","wow I am learning so much great stuff", "wow I am learning so much great stuff!", "wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!"]
+    paragraph = ['art work' for x in range(5)] 
 
     try:
         return render_template("index.html", title = title, paragraph=paragraph)
     except Exception, e:
         return str(e)
 
-@app.route('/about/contact')
-def contactPage():
-
-    title = "About this site"
-    paragraph = ["blah blah blah memememememmeme blah blah memememe"]
-
-    pageType = 'about'
-
-    return render_template("index.html", title=title, paragraph=paragraph, pageType=pageType)
-
-
- 
 if __name__ == "__main__":
   app.run(debug = True, host='0.0.0.0', port=5000, passthrough_errors=True)
