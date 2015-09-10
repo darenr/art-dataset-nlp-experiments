4539d2e09f791ff5bf88c7a18286e87700cbf703
daren race <daren.race@gmail.com>
2015-09-10 00:08:54 -0700

    fixed bug in turning synset back to word for wv
    also temp removed the wordnet synonyms to evaluate new wv model

--- term_expansions.py
+++ term_expansions.py
@@ -3,19 +3,21 @@ from nltk.corpus.reader import NOUN
 from gensim.models import Word2Vec
 import os
 
-model = Word2Vec.load_word2vec_format(os.environ['HOME'] + "/models/en_deps_300D_words.model")
+#model = Word2Vec.load_word2vec_format(os.environ['HOME'] + "/models/en_deps_300D_words.model")
+model = Word2Vec.load_word2vec_format(os.environ['HOME'] + "/models/en_bow5_300d.model")
 #model = Word2Vec.load_word2vec_format(os.environ['HOME'] + "/models/glove.42B.300d.txt", binary=False)
 def wv_synonyms(word):
+  print 'expanding', word
   if word in model:
-    return [x[0] for x in model.most_similar(word)[:5]]
+    return [x[0] for x in model.most_similar(word)[:9]]
   else:
     return []
 
 def wn_synonyms(ss):
   return [l.name().decode('utf-8') for l in ss.lemmas()]
 
-def wn_getword(s):
-  return ss.name().decode('utf-8')
+def wn_getword(ss):
+  return ss if isinstance(ss, basestring) else ss.name().decode('utf-8').split('.')[0]
 
 def wn_make_synset(word):
   if '.' in word:
@@ -27,17 +29,16 @@ def wn_make_synset(word):
     else:
       return None
 
-for word in ["figuration", "gay.n.01", "portraiture.n.01", "folk_art.n.01", "gender_identity.n.01", "gender.n.02"]:
+for word in [ "gay.n.01", "taboo", "portraiture.n.01", "folk_art.n.01", "gender_identity.n.01", "gender.n.02"]:
   
 
   synonyms = []
 
   ss = wn_make_synset(word)
-  if ss:
-    synonyms += wn_synonyms(ss)
+  #if ss:
+  #  synonyms += wn_synonyms(ss)
 
   synonyms += wv_synonyms(wn_getword(ss) if ss else word)
-
 
   synonyms = list(set(synonyms))
 
