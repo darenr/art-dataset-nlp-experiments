533be2453ab08b8eb62ea7e813c51eac901c6952
Lincoln Nguyen <lincolnnguyen@myhost.local>
2015-09-22 12:50:31 -0700

    ui changes to multifacet search

--- webapp/app.py
+++ webapp/app.py
@@ -118,7 +118,7 @@ def homepage():
         }
       }   
 
-    # Multifacet Search TODO: use list comprehension
+    # TODO: use list comprehension
     if request.method == 'POST':      
       formData = request.values      
 
