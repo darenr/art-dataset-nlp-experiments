6c6132418f952a22e7e46464977631b58abf3a9f
Lincoln Nguyen <lincolnnguyen@myhost.local>
2015-09-29 17:37:43 -0700

    maintain checkbox state

--- webapp/static/js/main.js
+++ webapp/static/js/main.js
@@ -67,4 +67,30 @@ $(function() {
   });
 
 
+  // Maintain State
+  $(":checkbox").on("change", function(){
+    var checkboxValues = {};
+    $(":checkbox").each(function(){
+      checkboxValues[this.id] = this.checked;
+    });
+      
+    $.cookie('checkboxValues', checkboxValues, { expires: 7, path: '/' });
+
+  });
+
+  function repopulateCheckboxes(){    
+    var checkboxValues = $.cookie('checkboxValues');    
+    if(checkboxValues){    
+
+      Object.keys(checkboxValues).forEach(function(element) {    
+        var checked = checkboxValues[element];
+        $("#" + element).prop('checked', checked);
+      });
+    }
+  }
+
+  $.cookie.json = true;
+  repopulateCheckboxes();
+
 });
+
