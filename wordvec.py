968264568b9a49c23029e69b5c29a50143bc6c6b
daren race <daren.race@gmail.com>
2015-10-03 12:57:53 -0700

    minor UI change
    comiting before starting something new

--- wordvec.py
+++ wordvec.py
@@ -3,6 +3,6 @@ from gensim.models import Word2Vec
 
 model = Word2Vec.load_word2vec_format(os.environ['HOME'] + "/models/en_deps_300D_words.model")
 
-for word in ["symbology", "dyslexia", "Language", "tactility", "communication", "tautology", "braille", "sight", "equations"]:
+for word in ["gender_issues", "lgbt", "gender", "light", "blackness"]:
   if word in model:
     print word, model.most_similar(word)
