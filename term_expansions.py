4a7e23500484281100ad144bbe215d97f227997b
daren race <daren.race@gmail.com>
2015-11-09 20:40:15 -0800

    word vec work

--- term_expansions.py
+++ term_expansions.py
@@ -3,9 +3,11 @@ from nltk.corpus.reader import NOUN
 from gensim.models import Word2Vec
 import os
 
-#model = Word2Vec.load_word2vec_format(os.environ['HOME'] + "/models/en_deps_300D_words.model")
-model = Word2Vec.load_word2vec_format(os.environ['HOME'] + "/models/en_bow5_300d.model")
+model = Word2Vec.load_word2vec_format(os.environ['HOME'] + "/models/en_deps_300D_words.model")
+#model = Word2Vec.load_word2vec_format(os.environ['HOME'] + "/models/en_bow5_300d.model")
 #model = Word2Vec.load_word2vec_format(os.environ['HOME'] + "/models/glove.42B.300d.txt", binary=False)
+#model = Word2Vec.load_word2vec_format(os.environ['HOME'] + "/models/freebase-vectors-skipgram1000-en.bin.gz", binary=True)
+
 def wv_synonyms(word):
   print 'expanding', word
   if word in model:
@@ -15,33 +17,9 @@ def wv_synonyms(word):
   else:
     return []
 
-def wn_synonyms(ss):
-  return [l.name().decode('utf-8') for l in ss.lemmas()]
-
-def wn_getword(ss):
-  return ss if isinstance(ss, basestring) else ss.name().decode('utf-8').split('.')[0]
-
-def wn_make_synset(word):
-  if '.' in word:
-    return wn.synset(word)
+for word in [ "cognitive dissonance", "political science", "urban development", "industrial development", "economic development"]:
+  if len(word.split()) > 1:
+    print wv_synonyms(word.replace(' ', '-'))
+    print wv_synonyms(word.replace(' ', '_'))
   else:
-    ss = wn.synsets(word, NOUN)
-    if ss:
-      return ss[0]
-    else:
-      return None
-
-for word in [ "gay.n.01", "war", "battle", "taboo", "portraiture.n.01", "folk_art.n.01", "gender_identity.n.01", "gender.n.02"]:
-  
-
-  synonyms = []
-
-  ss = wn_make_synset(word)
-  if ss:
-    synonyms += wn_synonyms(ss)
-
-  synonyms += wv_synonyms(wn_getword(ss) if ss else word)
-  
-  synonyms = list(set(synonyms))
-
-  print word, synonyms
+    print wv_synonyms(word)
