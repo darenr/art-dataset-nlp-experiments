a38b0670b85502f0ce2080102150d6f6e6e88732
darenr <daren.race@gmail.com>
2015-11-10 15:58:03 -0800

    use this model

--- term_expansions.py
+++ term_expansions.py
@@ -3,13 +3,14 @@ from nltk.corpus.reader import NOUN
 from gensim.models import Word2Vec
 import os
 
-#model = Word2Vec.load_word2vec_format(os.environ['HOME'] + "/models/en_deps_300D_words.model")
+model = Word2Vec.load_word2vec_format(os.environ['HOME'] + "/models/en_deps_300D_words.model")
 #model = Word2Vec.load_word2vec_format(os.environ['HOME'] + "/models/en_bow5_300d.model")
-model = Word2Vec.load_word2vec_format(os.environ['HOME'] + "/models/glove.42B.300d.txt", binary=False)
+#model = Word2Vec.load_word2vec_format(os.environ['HOME'] + "/models/glove.42B.300d.txt", binary=False)
+#model = Word2Vec.load_word2vec_format(os.environ['HOME'] + "/models/GoogleNews-vectors-negative300.bin", binary=True)
+#model = Word2Vec.load_word2vec_format(os.environ['HOME'] + "/models/freebase-vectors-skipgram1000-en.bin", binary=True)
 
 def wv_synonyms(word):
   w = '_'.join(word.split(' '))
-  print 'expanding', word
   if w in model:
     # results are not sorted, so before we truncate let's sort them,
     sorted_sims = sorted(model.most_similar(w), key=lambda tup: tup[1])
@@ -17,5 +18,5 @@ def wv_synonyms(word):
   else:
     return []
 
-for word in [ "globalization", "landscape", "abstraction", "appropriation"]:
-  print word, wv_synonyms(word)
+for word in [ "pollution", "urbanization", "reclamation", "globalization", "landscape", "abstraction", "appropriation"]:
+  print word, wv_synonyms(word), "\n\n"
