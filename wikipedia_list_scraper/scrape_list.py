7b16061089505c1808b6b410eb8b41c22181425e
daren race <daren.race@gmail.com>
2015-11-18 20:28:06 -0800

    added more scraping

--- wikipedia_list_scraper/scrape_list.py
+++ wikipedia_list_scraper/scrape_list.py
@@ -4,7 +4,7 @@ from urlparse import urljoin
 import re
 
 
-def scrape(url):
+def scrape_type1(url):
   page = urllib2.urlopen(url)
   soup = BeautifulSoup(page.read(), 'html5lib')
   p = re.compile(r'\(.*\)')
@@ -12,6 +12,17 @@ def scrape(url):
     #print urljoin(url, a['href'])
     print p.sub('', a['title'])
 
+def scrape_type2(url):
+  page = urllib2.urlopen(url)
+  soup = BeautifulSoup(page.read(), 'html5lib')
+  p = re.compile(r'\(.*\)')
+  for a in soup.find_all("div", class_="mw-content-ltr")[0].find_all("a", attrs = {'title' : True}):
+    if not '.php' in a['href']:
+      #print urljoin(url, a['href'])
+      print p.sub('', a['title']), a.parent.contents[1]
+
 
 if __name__ == "__main__":
-  scrape('https://en.wikipedia.org/wiki/Category:Conceptual_artists')
+  #scrape_type1('https://en.wikipedia.org/wiki/Category:American_conceptual_artists')
+  #scrape_type1('https://en.wikipedia.org/wiki/Category:Conceptual_artists')
+  scrape_type2('https://en.wikipedia.org/wiki/List_of_contemporary_artists')
