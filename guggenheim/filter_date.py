3aaf25e9b4004c82b408b60806e4094835ca25f7
daren race <daren.race@gmail.com>
2015-12-08 10:20:46 -0800

    added guggenheim processor

---
+++ guggenheim/filter_date.py
@@ -0,0 +1,16 @@
+import unicodedata
+import json
+import codecs
+
+with codecs.open('postwar_unicode_guggenheim.json', 'wb', 'utf-8') as fout:
+    results = []
+    with codecs.open('unicode_guggenheim.json', 'rb', 'utf-8') as fin:
+        guggenheim = json.loads(fin.read())
+        for map in guggenheim:
+          if 'date' in map: 
+            try:
+              if int(map['date']) >= 1945:
+                results.append(map)
+            except:
+              pass
+    fout.write(json.dumps(results, indent=2, sort_keys=True, ensure_ascii=False))
